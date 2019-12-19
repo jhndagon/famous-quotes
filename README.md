54456aadca172c0108f0fb0e960ec7bc7b33f784
John David Gonzalez <jhndagon@gmail.com>
2019-12-19 10:54:07 -0500

    Changes in README file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # famouse-quotes
+
+## Run
+1. Execute command npm install
+2. Configure MONGODB_URL
+3. Execute node app.js
--- config/db.js
+++ config/db.js
@@ -1,1 +1,3 @@
-const MONGODB_URL = process.env.MONGODB | 'mongodb+srv://<username>:<password>@cluster0-0wx6b.mongodb.net/test?retryWrites=true&w=majority'
+const MONGODB_URL = process.env.MONGODB || 'mongodb+srv://<username>:<password>@cluster0-0wx6b.mongodb.net/test?retryWrites=true&w=majority'
+
+module.exports = MONGODB_URL;
